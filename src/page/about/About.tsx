d87117bcd637d1456ef34e0c67233f517d8ed105
niriniarisoa <muriellaniriniarisoa@gmail.com>
2025-03-06 12:40:00 +0100

    react_dom

---
+++ src/page/about/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>À propos</h1>
+      <p>Ceci est la page "À propos".</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/page/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './Home.css'; // Styles spécifiques à la page (optionnel)
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur la page d'accueil</h1>
+      <p>Ceci est un exemple de page.</p>
+    </div>
+  );
+};
+
+export default Home;
